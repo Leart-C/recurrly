f3df1f7f0ad2d43d557c933e2d706068289dbdc6
Leart Bajrami <73801402+Leart-C@users.noreply.github.com>
2026-07-28 00:40:19 +0200

    Update app/_layout.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,6 @@
 import { SplashScreen, Stack } from "expo-router";
-import '@/global.css';
+void SplashScreen.preventAutoHideAsync();
+import '`@/global.css`';
 import { useFonts } from 'expo-font';
 import { useEffect } from "react";
 
